9880ea784c208a3de4bfb9657f0269c30deff278
Sterzinha <stermiss2@gmail.com>
2024-01-07 18:01:10 -0400

    teste

--- src/components/Participant/index.tsx
+++ src/components/Participant/index.tsx
@@ -10,7 +10,7 @@ export function Participant({ name, onRemove }: Props) {
   return (
     <View style={styles.container}>
       <Text style={styles.name}>{name}</Text>
-
+      kkkk teste
       <TouchableOpacity style={styles.button} onPress={onRemove}>
         <Text style={styles.buttonText}>-</Text>
       </TouchableOpacity>
